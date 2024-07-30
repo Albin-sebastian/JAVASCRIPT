e0d334ead90024ca8a9f4517a4e28abf38128fc8
Albin sebastian <albinsebastianmca@gmail.com>
2024-07-30 08:53:06 +0530

    formData API added

--- FORM-DATA/script.js
+++ FORM-DATA/script.js
@@ -1,42 +1,11 @@
-const formData = new FormData();
-console.log(formData);
-// append:ƒ append()
-// delete:ƒ delete ()
-// entries:ƒ entries()
-// forEach:ƒ forEach()
-// get:ƒ()
-// getAll:ƒ getAll()
-// has:ƒ has()
-// keys:ƒ keys()
-// set:ƒ()
-// values:ƒ values()
+const form = document.getElementById("form");
+const btn = document.getElementById("btn");
 
-formData.append("name", "john");
-formData.append("email", "john@gmail.com");
-
-// console.log(formData.get('name'));
-// console.log(formData.get('email'));
-
-formData.append("name", "peter");
-formData.append("name", "else");
-// console.log(formData.getAll('name'));
-
-// console.log(formData.has('name'));
-// console.log(formData.keys());
-// we use iterator
-// for (let key of formData.keys()) {
-//   console.log(key);
-// }
-
-// for (let v of formData.values()) {
-//   console.log(v);
-// }
-
-// for (let keyValue of formData.entries()) {
-//   console.log(keyValue);
-// }
-formData.delete('phone');
-
-for (let keyValue of formData.entries()) {
-  console.log(keyValue[0], ":", keyValue[1]);
-}
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  // console.log(formData);
+  for (const keyValue of formData.entries()) {
+    console.log(keyValue[0], ":", keyValue[1]);
+  }
+})
